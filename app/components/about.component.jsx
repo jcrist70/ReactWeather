2ba826c09e9d3aab0234cfbe60a55e7c756d5098
jcrist70 <jcrist70@gmail.com>
2019-10-07 04:09:27 -0700

    Styled about page

--- app/components/about.component.jsx
+++ app/components/about.component.jsx
@@ -2,7 +2,25 @@ const React = require('react');
 
 const About = () => {
     return (
-        <h3>About Component</h3>
+        <div>
+            <h3 className="text-center">About</h3>
+            <p>This is a weather app build on React using Web API's.</p>
+            <p>Some related links:</p>
+            <ul>
+                <li>
+                    <a href="https://facebook.github.io/react">JS React Framework</a>
+                </li>
+                <li>
+                    <a href="https://openweathermap.org">Weathermap API for weather data</a>
+                </li>
+                <li>
+                    <a href="https://foundation.zurb.com">Zurb Foundation for styling</a>
+                </li>
+                <li>
+                    <a href="https://www.zipcodeapi.com/">City to zipcode API</a>
+                </li>
+            </ul>
+        </div>
     );
 }
 

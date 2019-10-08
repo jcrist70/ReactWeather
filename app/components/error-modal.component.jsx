bd73f9731992af06e5ec07a79af839166c6ba9e5
jcrist70 <jcrist70@gmail.com>
2019-10-08 08:25:55 -0700

    fixed modal conflict

--- app/components/error-modal.component.jsx
+++ app/components/error-modal.component.jsx
@@ -1,4 +1,6 @@
 const React = require('react');
+const ReactDOM = require('react-dom');
+const ReactDOMServer = require('react-dom/server');
 
 const ErrorModal = React.createClass({
     getDefaultProps: function () {
@@ -11,12 +13,8 @@ const ErrorModal = React.createClass({
     //     message: React.propTypes.string.isRequired
     // },
     componentDidMount: function () {
-        var modal = new Foundation.Reveal($('#error-modal'));
-        modal.open();
-    },
-    render: function () {
         var { title, message } = this.props;
-        return (
+        var modalMarkup = (
             <div id="error-modal" className="reveal tiny text-center" data-reveal="">
                 <h4>{title}</h4>
                 <p>{message}</p>
@@ -24,6 +22,15 @@ const ErrorModal = React.createClass({
                     <button className="button hollow" data-close="">OK</button>
                 </p>
             </div>
+        );
+        var $modal = $(ReactDOMServer.renderToString(modalMarkup));
+        $(ReactDOM.findDOMNode(this)).html($modal);
+        var modal = new Foundation.Reveal($('#error-modal'));
+        modal.open();
+    },
+    render: function () {
+        return (
+            <div></div>
         );
     }
 });
